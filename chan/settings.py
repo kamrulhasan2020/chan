30cba60a98938dd1d015211aa92984537f6f3bf3
kamrul <kkamrulhasan2020@gmail.com>
2022-02-22 20:24:12 +0600

    added postgresql as default

--- chan/settings.py
+++ chan/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'chan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypass',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
